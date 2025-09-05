4ba151a779a8ec85716060281d4f30ce3ffd0782
abhinavkumarsph <122246745+abhinavkumarsph@users.noreply.github.com>
2025-09-05 11:46:53 +0800

    preparation for aws provider v6 upgrade (#6)
    * preparation for aws provider v6 upgrade
    
    * chore: minor refactor
    
    * fix: ci workflow path
    
    * fix: ci workflow
    
    * fix: ci workflow
    
    * chroe: revert ci workflow
    
    * chore: renamed ci jobs

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-data "aws_caller_identity" "current" {
-}
-
 locals {
   account_id = data.aws_caller_identity.current.account_id
 }
@@ -19,33 +16,4 @@ resource "aws_kms_alias" "key" {
 
   name          = var.alias
   target_key_id = aws_kms_key.key.key_id
-}
-
-#####################################
-# Allow IAM access to keys
-# See https://docs.aws.amazon.com/kms/latest/developerguide/key-policies.html
-# for more information on key policies
-#####################################
-data "aws_iam_policy_document" "iam_key_policy" {
-  #checkov:skip=CKV_AWS_109:The policy is deliberately wide because it allows IAM control of the KMS
-  #checkov:skip=CKV_AWS_111:The policy is deliberately wide because it allows IAM control of the KMS
-  #checkov:skip=CKV_AWS_356:The policyi s kept wide for IAM control of KMS
-  statement {
-    sid = "Allow administration of the key by key creator account"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${local.account_id}:root"]
-    }
-
-    actions   = ["kms:*"]
-    resources = ["*"]
-  }
-}
-
-data "aws_iam_policy_document" "combined_key_policy" {
-  source_policy_documents = concat(
-    [data.aws_iam_policy_document.iam_key_policy.json],
-    var.key_policy_statements,
-  )
 }
